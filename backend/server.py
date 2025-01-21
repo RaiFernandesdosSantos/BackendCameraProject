e204b513b6b76c3d8d0056ed7a99ca80a7d65f9d
RaiFernandesdosSantos <raifernandes076@gmail.com>
2025-01-20 21:08:20 -0400

    Init

--- backend/server.py
+++ backend/server.py
@@ -1,36 +1,32 @@
 import cv2
 import PIL.Image
 import google.generativeai as genai
-from docx import Document
-from flask import Flask
+from flask import Flask, request
 
 
-genai.configure(api_key="AIzaSyDqxdaEYCu3z727HXeJx3VRwyjeWkuV7e8")
+genai.configure(api_key="API_KEY")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-image_path = "./assets/images.jpg"
-image = cv2.imread(image_path)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-_, thresh = cv2.threshold(gray, 102, 255, cv2.THRESH_BINARY_INV)
-
-cv2.imwrite("imagem_processada.png", thresh)
+app = Flask(__name__)
 
 
-text = PIL.Image.open("imagem_processada.png")
-response = model.generate_content(
-    ["O que está escrito? apenas transcreva o texto sem comentar nada", text]
-)
-print(response.text)
+@app.route("/catch", methods=["POST"])
+def catch_image():
+    image = request.files["image"]
 
-doc = Document()
-style = doc.styles["Normal"]
-font = style.font
-font.name = "Times New Roman"
-text_doc = response.text
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-doc.add_paragraph(text_doc)
+    _, thresh = cv2.threshold(gray, 102, 255, cv2.THRESH_BINARY_INV)
 
-doc.save("document.docx")
+    cv2.imwrite("imagem_processada.png", thresh)
 
-print("Convertido com sucesso!")
+    text = PIL.Image.open("imagem_processada.png")
+    response = model.generate_content(
+        ["O que está escrito? apenas transcreva o texto sem comentar nada", text]
+    )
+    return response.text
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
